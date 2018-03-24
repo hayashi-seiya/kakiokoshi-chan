cc87b765340408cdcbd27a82ea601d6e3f0dd32b
Seiya Hayashi <shayashi@hitomedia.jp>
2018-03-24 18:28:49 +0900

    tmp/audio配下にあるwavファイルを書き起こしできるように

--- app/models/google_speech_api.rb
+++ app/models/google_speech_api.rb
@@ -2,9 +2,21 @@ class GoogleSpeechAPI
   require "google/cloud/speech"
   include Singleton
 
-  PROJECT_ID = "kakiokoshi-chan"
+  PROJECT_ID = "kakiokoshi-chan".freeze
 
   def initialize
     @speech = Google::Cloud::Speech.new(project: PROJECT_ID)
+  end
+
+  def transcript
+    audio_dir = Rails.root.join("tmp", "audio")
+    audio_file_name = "#{audio_dir}/0101001.wav"
+    audio = @speech.audio(audio_file_name,
+                          encoding: :linear16,
+                          sample_rate: 44100,
+                          language: "ja")
+
+    results = audio.recognize
+    results.first.transcript
   end
 end
